96fca40c8cebb58f136d45300bcae9986793f1d4
Akanksha-Shahi <akankshashahi@gmail.com>
2026-06-25 20:33:21 +0530

    "Add SortNumbers.cpp"

---
+++ SortNumbers.cpp
@@ -0,0 +1,20 @@
+// Given three numbers A, B, C. Print these numbers in ascending order followed by a blank line and then the values in the sequence as they were read.
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int x=a, y=b, z=c;
+    if( a>b) swap(a,b);
+    if(b>c) {
+        swap(b,c);
+        if(a>b) swap(a,b);
+    }
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<< " "<<endl;
+    cout<<x<<endl;
+    cout<<y<<endl;
+    cout<<z<<endl;
+}
