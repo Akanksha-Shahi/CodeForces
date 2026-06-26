656de88a264a821a5902a7b76460a4221b1b5499
Akanksha-Shahi <akankshashahi@gmail.com>
2026-06-26 16:21:42 +0530

    Add Brothers

---
+++ Brothers.cpp
@@ -0,0 +1,13 @@
+// Given two person names.Each person has {"the first name" + "the second name"} Determine whether they are brothers or not.
+// Note: The two persons are brothers if they share the same second name.
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string F1, S1, F2, S2;
+    cin>> F1 >> S1;
+    cin>> F2 >> S2;
+    if ( S1 == S2) cout<<"ARE Brothers";
+    else cout<<"NOT";
+    return 0;
+}
