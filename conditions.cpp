cbb238a935bdfa1b5105c82561d48343ec2a29ee
Akanksha-Shahi <akankshashahi@gmail.com>
2026-06-26 17:28:31 +0530

    Add Conditions

---
+++ conditions.cpp
@@ -0,0 +1,9 @@
+// Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
+#include<iostream>
+using namespace std;
+int main(){
+    int A, B;
+    cin>> A >>B;
+    if( A>=B) cout<<"Yes";
+    else cout<<"No";
+}
