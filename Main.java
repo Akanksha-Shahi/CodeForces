6454801396fd49b140b0ad925b4b0db38b1378ae
Akanksha Shahi <akankshashahi2005@gmail.com>
2026-07-13 19:46:29 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextLong();
+        long result = (long)Math.ceil(x/5.0);
+        System.out.println(result);
+        sc.close();
+    }
+}
