ebcfde5b22b63becfc577a2aa4acab32649618bc
Akanksha Shahi <akankshashahi2005@gmail.com>
2026-07-14 16:11:27 +0530

    Create Domino.java

---
+++ Domino.java
@@ -0,0 +1,11 @@
+// 50A
+import java.util.Scanner;
+public class Domino{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println((m*n)/2);
+        sc.close();
+    }
+}
