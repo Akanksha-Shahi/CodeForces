8a19806d779e570e80416fb3442b5acfb362d628
Akanksha Shahi <akankshashahi2005@gmail.com>
2026-07-16 23:24:44 +0530

    Update DivideConquer.java

--- DivideConquer.java
+++ DivideConquer.java
@@ -2,16 +2,16 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String args[]){
-        Scanner sc = new sc.nextInt(System.in);
-        if(sc.hasNextInt(){
-            int t = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.hasNextInt()){
+            int t = scanner.nextInt();
             for( int i =0; i<t; i++){
-                int x = sc.nextInt();
-                int y = sc.nextInt();
+                int x = scanner.nextInt();
+                int y = scanner.nextInt();
                 if(x%y==0) System.out.println("YES");
                 else System.out.println("NO");
             }
         } 
-        sc.close();
+        scanner.close();
     }
 }
