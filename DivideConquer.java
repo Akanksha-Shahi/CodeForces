0f92c855e3a309b680df675bd57835dc8fff0bd2
Akanksha Shahi <akankshashahi2005@gmail.com>
2026-07-16 23:14:52 +0530

    Create DivideConquer.java

---
+++ DivideConquer.java
@@ -0,0 +1,17 @@
+// 2241A
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new sc.nextInt(System.in);
+        if(sc.hasNextInt(){
+            int t = sc.nextInt();
+            for( int i =0; i<t; i++){
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                if(x%y==0) System.out.println("YES");
+                else System.out.println("NO");
+            }
+        } 
+        sc.close();
+    }
+}
